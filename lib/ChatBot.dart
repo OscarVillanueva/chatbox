ecfdc5a42fbb36e14ba649595ba0a04995642d75
Oscar Villanueva <javiondo68@gmail.com>
2021-01-16 15:39:38 -0600

    app done

--- lib/ChatBot.dart
+++ lib/ChatBot.dart
@@ -1,6 +1,8 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class ChatBot extends StatefulWidget {
   ChatBot({Key key}) : super(key: key);
@@ -10,20 +12,69 @@ class ChatBot extends StatefulWidget {
 }
 
 class _ChatBotState extends State<ChatBot> {
+  final txtMessageController = TextEditingController();
+  List<Map> messages = List();
+  bool reset = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Restaurate El tortas"),
+        title: Text("Restaurate Chatbox"),
         backgroundColor: Colors.orange,
+        actions: <Widget>[
+          MaterialButton(
+              child: Icon(Icons.reset_tv, color: Colors.white),
+              onPressed: () => resetConversation())
+        ],
       ),
-      body: Container(
-        child: Center(
-          child: RaisedButton(
-            onPressed: () {
-              response("hola");
-            },
-            child: Text("response"),
+      body: LoadingOverlay(
+        isLoading: reset,
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                  child: ListView.builder(
+                      // reverse: true,
+                      padding: EdgeInsets.all(15.0),
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) => generateBubble(
+                          messages[index]["data"],
+                          messages[index]["message"]))),
+              Divider(height: 3.0),
+              Container(
+                padding: EdgeInsets.only(bottom: 10.0),
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                        child: TextField(
+                            controller: txtMessageController,
+                            decoration: InputDecoration.collapsed(
+                                hintText: "Enviar mensaje"))),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                          icon: Icon(Icons.send),
+                          onPressed: () {
+                            if (txtMessageController.text.isEmpty)
+                              print("vacio");
+                            else {
+                              setState(() {
+                                messages.add({
+                                  "data": 1,
+                                  "message": txtMessageController.text
+                                });
+                              });
+                              response(txtMessageController.text);
+                              txtMessageController.clear();
+                            }
+                          }),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
@@ -37,8 +88,43 @@ class _ChatBotState extends State<ChatBot> {
     Dialogflow dialogflow =
         await Dialogflow(authGoogle: authGoogle, language: Language.english);
 
-    AIResponse aiResponse = await dialogflow.detectIntent('hola');
+    AIResponse aiResponse = await dialogflow.detectIntent(query);
 
-    print(aiResponse.getMessage());
+    setState(() {
+      messages.add({"data": 0, "message": aiResponse.getMessage()});
+    });
+  }
+
+  Widget generateBubble(side, message) {
+    return Bubble(
+      margin: BubbleEdges.only(top: 10),
+      nipWidth: 10,
+      nipHeight: 10,
+      nip: side == 0 ? BubbleNip.leftBottom : BubbleNip.rightBottom,
+      color: side == 0 ? Colors.grey[300] : Colors.orange,
+      child: Text(
+        message,
+        style: TextStyle(fontSize: 15.0),
+      ),
+      alignment: side == 0 ? Alignment.topLeft : Alignment.topRight,
+    );
+  }
+
+  void resetConversation() {
+    setState(() {
+      reset = true;
+    });
+
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        messages.clear();
+      });
+    });
+
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        reset = false;
+      });
+    });
   }
 }
